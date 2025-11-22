d9a4471e765c20649f33c7d2987ab9339d325f82
netizen-0 <rishabhytpremium17@gmail.com>
2025-11-22 14:28:31 +0530

    Update data.py

--- cache/data.py
+++ cache/data.py
@@ -205,13 +205,13 @@ RAID = [
 ]
 
 
-GRP = "@WORLD_ALPHA"
+GRP = "@Yaaro_kimehfill"
 
-GROUP = [-1002094067251]
+GROUP = [-1002427493136]
 
-VERIFIED_USERS = [6730956183]
+VERIFIED_USERS = [5011400467]
 
-Owners = "@ll_ALPHA_BABY_lll|@II_RAJA_BABU_II"
+Owners = "@medevu|@ptsel"
 
 PORN = [
         "https://telegra.ph/file/9bcc076fd81dfe3feb291.mp4",
